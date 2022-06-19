75ecfc6aa11137ef015edee8d6dca8e303742485
LostMoa <life4458@naver.com>
2022-06-20 01:43:31 +0900

    jump 수정

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    void OnTriggerStay(Collider other)
+    {
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    const int soundSize = 2;
     const float doubleInput = 0.7071f;
     public static Player instance;
-    public float mvspd = 2f, mvspd_;
-    public float jumpPower = 175f;
+    public float mvspd_ = 2f, mvspd = 2f;
+    public float mvsnd = 1f;
+    public int jumpPower = 100;
+    bool move_able = true;
+    bool jump_able = true;
+    bool mvsnd_able = true;
+    [SerializeField] bool isMoving = false;
     [SerializeField] bool grounded = true;
     GameObject mainCamera;
     GameObject hand;
     Rigidbody rigid;
+    AudioSource audioSource;
+    [SerializeField] AudioClip[] audioClip = new AudioClip[soundSize];
     public bool Grounded { set { grounded = value; } }
+    enum AudioClipName
+    {
+        move, jump
+    }
     void Awake()
     {
         instance = this;
@@ -22,77 +34,122 @@ public class Player : MonoBehaviour
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
         hand = GameObject.FindGameObjectWithTag("Hand");
         rigid = gameObject.GetComponent<Rigidbody>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioClip[(int)AudioClipName.move] = Resources.Load<AudioClip>("SoundEffect/Player/Slime_Move");
+        audioClip[(int)AudioClipName.jump] = Resources.Load<AudioClip>("SoundEffect/Player/Slime_Jump");
     }
     void Update()
     {
-        // 대쉬
-        if(Input.GetKey(KeyCode.LeftShift))
+        if (jump_able)
         {
-            mvspd_ = mvspd * 1.5f;
-        }else
-        {
-            mvspd_ = mvspd;
+            Jump();
         }
-
-        // 점프
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        if (move_able && grounded)
         {
-            rigid.velocity = new Vector3(0, jumpPower * Time.deltaTime, 0);
-            grounded = false;
+            Sprint();
+            Move();
         }
-
-        // 8방향 이동
-        if(Input.GetKey(KeyCode.W)&&Input.GetKey(KeyCode.A))
+        if(isMoving)
         {
-            rigid.transform.Translate(new Vector3(-1 * doubleInput * mvspd_ * Time.deltaTime, 0, doubleInput * mvspd_ * Time.deltaTime), Space.World);
-            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, -45, 0));
-            return;
-        }
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
-        {
-            rigid.transform.Translate(new Vector3(doubleInput * mvspd_ * Time.deltaTime, 0, doubleInput * mvspd_ * Time.deltaTime), Space.World);
-            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, 45, 0));
-            return;
-        }
-        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
-        {
-            rigid.transform.Translate(new Vector3(-1 * doubleInput * mvspd_ * Time.deltaTime, 0, -1 * doubleInput * mvspd_ * Time.deltaTime), Space.World);
-            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, -135, 0));
-            return;
-        }
-        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
-        {
-            rigid.transform.Translate(new Vector3(doubleInput * mvspd_ * Time.deltaTime, 0, -1 * doubleInput * mvspd_ * Time.deltaTime), Space.World);
-            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, 135, 0));
-            return;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            rigid.transform.Translate(new Vector3(0, 0, mvspd_ * Time.deltaTime), Space.World);
-            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-            return;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rigid.transform.Translate(new Vector3(0, 0, -1 * mvspd_ * Time.deltaTime), Space.World);
-            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-            return;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rigid.transform.Translate(new Vector3(-1 * mvspd_ * Time.deltaTime, 0, 0), Space.World);
-            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
-            return;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rigid.transform.Translate(new Vector3(mvspd_ * Time.deltaTime, 0, 0), Space.World);
-            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
-            return;
+            if (mvsnd_able)
+            {
+                mvsnd_able = false;
+                // 플레이어 이동 사운드 재생
+                audioSource.clip = audioClip[(int)AudioClipName.move];
+                audioSource.Play();
+                StartCoroutine(MoveSound());
+            }
+            isMoving = false;
         }
     }
     void LateUpdate()
     {
-        mainCamera.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 14.5f, gameObject.transform.position.z - 12.5f);
+        mainCamera.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 10.5f, gameObject.transform.position.z - 8.5f);
+    }
+    IEnumerator MoveSound()
+    {
+        yield return new WaitForSeconds(Random.Range(mvsnd * 0.5f, mvsnd * 1.2f));
+        mvsnd_able = true;
+    }
+    void Sprint()
+    {
+        // 달리기
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            mvspd = mvspd_ * 1.5f;
+        }
+        else
+        {
+            mvspd = mvspd_;
+        }
+    }
+    void Jump()
+    {
+        // 점프
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+            // 플레이어 점프 사운드 재생
+            audioSource.clip = audioClip[(int)AudioClipName.jump];
+            audioSource.Play();
+            grounded = false;
+        }
+    }
+    void Move()
+    {
+        // 8방향 이동
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            rigid.velocity = new Vector3(-1 * doubleInput * mvspd, 0, doubleInput * mvspd);
+            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, -45, 0));
+            isMoving = true;
+            return;
+        }
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            rigid.velocity = new Vector3(doubleInput * mvspd, 0, doubleInput * mvspd);
+            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, 45, 0));
+            isMoving = true;
+            return;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            rigid.velocity = new Vector3(-1 * doubleInput * mvspd, 0, -1 * doubleInput * mvspd);
+            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, -135, 0));
+            isMoving = true;
+            return;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            rigid.velocity = new Vector3(doubleInput * mvspd, 0, -1 * doubleInput * mvspd);
+            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, 135, 0));
+            isMoving = true;
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigid.velocity = new Vector3(0, 0, mvspd);
+            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            isMoving = true;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rigid.velocity = new Vector3(0, 0, -1 * mvspd);
+            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            isMoving = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigid.velocity = new Vector3(-1 * mvspd, 0, 0);
+            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+            isMoving = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigid.velocity = new Vector3(mvspd, 0, 0);
+            rigid.transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+            isMoving = true;
+        }
     }
 }
