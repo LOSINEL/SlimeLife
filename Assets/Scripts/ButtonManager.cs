88bc6e37b05f6b9fba9f87c416fd0dc571f769e9
LostMoa <life4458@naver.com>
2022-06-27 19:55:58 +0900

    사운드 조정 옵션 추가
    사운드 매니저를 따로 생성하였음

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public static ButtonManager instance;
+    GameObject optionCanvas;
+    GameObject grayCanvas;
+    bool isOptionCanvasOpen = false;
+    public bool IsOptionCanvasOpen { get { return isOptionCanvasOpen; } }
+    void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "InGame")
+        {
+            optionCanvas = GameObject.Find("Option_Canvas");
+            grayCanvas = GameObject.Find("Gray_Canvas");
+            SetOptionCanvas();
+        }
+    }
+    public void SetOptionCanvas()
+    {
+        grayCanvas.SetActive(!optionCanvas.activeSelf);
+        optionCanvas.SetActive(!optionCanvas.activeSelf);
+        isOptionCanvasOpen = optionCanvas.activeSelf;
+    }
+}
